a77ee2931c2d8e3c377c1280549f13113a966578
sergio0006 <sergio.ana@aluno.faculdadeimpacta.com.br>
2023-09-12 16:51:30 -0300

    views de login concluida com sucesso e feita a verificação.

--- main/views/login.py
+++ main/views/login.py
@@ -1,10 +1,30 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from ..forms.login_forms import LoginForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 
 class Login(View):
     def get(self, request):
         form = LoginForm
         formulario = {'form': form}
+
         return render(request, 'login.html', formulario)
+    
+    def post(self, request):
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+
+                return redirect('dashboard')
+            
+            else:
+                messages.error(request, 'Credenciais invalidas.')
+
+                return render(request, 'login.html')
