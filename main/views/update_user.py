93c8a1294ef754c54fcb83e9c84a43a63514b69f
sergio0006 <sergio.ana@aluno.faculdadeimpacta.com.br>
2023-09-14 16:11:45 -0300

    colocando o LoginRequiredMixin dentro da view para somente usuários cadastrados poderem acessar ela.

--- main/views/update_user.py
+++ main/views/update_user.py
@@ -2,9 +2,10 @@ from django.views.generic.edit import UpdateView
 from ..models import Usuario
 from ..forms.updateuser_forms import UserUpdateForm
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class UpdateUser(UpdateView):
+class UpdateUser(LoginRequiredMixin, UpdateView):
     model = Usuario
     form_class = UserUpdateForm
     template_name = 'update_user.html'
