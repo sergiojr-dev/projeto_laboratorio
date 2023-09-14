a46d7db3eded7fdaccc6bd2a57b3e2a911067dd3
sergio0006 <sergio.ana@aluno.faculdadeimpacta.com.br>
2023-09-14 11:39:06 -0300

    atualização da logica de verificação do usuário e do POST

--- main/views/login.py
+++ main/views/login.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from ..forms.login_forms import LoginForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 
 
 class Login(View):
@@ -13,19 +14,31 @@ class Login(View):
         return render(request, 'login.html', formulario)
     
     def post(self, request):
-        form = LoginForm(request.POST)
+        if request.method == "POST":
+            form = LoginForm(request, request.POST)
+        #form = LoginForm(request.POST)
+        print('PARADA 2')
+        
         if form.is_valid():
+            print('PARADA 3')
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
 
+            print(f'FORMS VALIDADO {username} e {password}')
+            print('PARADA 2')
+
             user = authenticate(request, username=username, password=password)
+            print(f'{user}, login.py TESTE')
+            print('PARADA 3')
 
             if user is not None:
                 login(request, user)
-
                 return redirect('dashboard', username=request.user.username)
-            
-        messages.error(request, 'Credenciais invalidas.')
-        form = LoginForm()
-        formulario = {'form': form}
-        return render(request, 'login.html', formulario)
+                
+            else:
+                print("Credenciais inválidas")
+                messages.error(request, 'Credenciais invalidas.')
+        else:
+            form = LoginForm()
+            formulario = {'form': form}
+            return render(request, 'login.html', formulario)
