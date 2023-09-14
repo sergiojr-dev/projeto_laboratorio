d72aa2482095d70524f1f3e34f14405ee4486f61
sergio0006 <sergio.ana@aluno.faculdadeimpacta.com.br>
2023-09-14 15:32:00 -0300

    inserção da url de update user.

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views.index import Index
 from .views.login import Login
 from .views.signup import Signup
-from .views.update_register import UpdateUser
+from .views.update_user import UpdateUser
 from .views.dashboard import Dashboard
 from .views.logout import Logout
 
@@ -13,5 +13,5 @@ urlpatterns = [
     path('logout/', Logout.as_view(), name='logout'),
     path('signup/', Signup.as_view(), name='signup'),
     path('dashboard/<str:username>/', Dashboard.as_view(), name='dashboard'),
-    #path('atualizar_usuario/<int:pk>', UpdateUser.as_view(), name='atualizar_usuario'),
+    path('atualizar/cadastro/<int:pk>', UpdateUser.as_view(), name='updateuser'),
 ]
