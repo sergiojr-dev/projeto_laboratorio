15a451101bbf3f677c481843612b77e2f2a5b606
sergio0006 <sergio.ana@aluno.faculdadeimpacta.com.br>
2023-09-14 15:48:03 -0300

    Transformando o is_active no forms em seleção ao inves de ser marcado.

--- main/forms/updateuser_forms.py
+++ main/forms/updateuser_forms.py
@@ -6,6 +6,13 @@ class UserUpdateForm(forms.ModelForm):
         (1, 'Aluno'),
         (2, 'Professor')
     ]
+
+    ATIVO_INATIVO = [
+            (1, 'Ativo'),
+            (2, 'Inativo')
+        ]
+    
+
     class Meta:
         model= Usuario
         fields = ['username', 'first_name', 'last_name', 'email', 'password', 'is_active', 'user', 'cpf_cnpj', 'phone']
@@ -15,7 +22,6 @@ class UserUpdateForm(forms.ModelForm):
             'last_name': forms.TextInput(attrs={'placeholder': 'Sobrenome'}),
             'email': forms.EmailInput(attrs={'placeholder': 'Endereço de email'}),
             'password': forms.PasswordInput(attrs={'placeholder': 'Senha'}),
-            'is_active': forms.CheckboxInput(attrs={'placeholder': 'Ativo'}),
             'cpf_cnpj': forms.TextInput(attrs={'placeholder': 'CPF/CNPJ'}),
             'phone': forms.TextInput(attrs={'placeholder': 'Telefone'}),
         }
@@ -24,4 +30,9 @@ class UserUpdateForm(forms.ModelForm):
     user = forms.ChoiceField(
         choices=[('', 'Selecione...')] + USUARIO,  # Adiciona "Selecione..." como a primeira opção
         widget=forms.Select(attrs={'placeholder': 'Usuário'})
+    )
+
+    is_active = forms.ChoiceField(
+        choices=[('', 'Usuário Ativo / Inativo')] + ATIVO_INATIVO,
+        widget=forms.Select(attrs={'placeholder': 'Ativo / Inativo'})
     )
