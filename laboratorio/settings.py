b009f778d160bb7960382aa7b869494c583e510d
sergio0006 <sergio.ana@aluno.faculdadeimpacta.com.br>
2023-09-15 21:38:00 -0300

    verificando o a funcionalidade das páginas de erro e configurando o ALLOWED_HOST com as variáveis de segurança

--- laboratorio/settings.py
+++ laboratorio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+a!=k9sojs!2d()(d7oog#$$1_d_0p!o@1+-2_lz_-_vlo!$ai'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True # Só pode colocar False em produção
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
 
 
 # Application definition
