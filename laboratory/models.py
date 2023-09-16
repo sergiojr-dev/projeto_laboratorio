e3a3a5f6ad33d8429b5567e8350f307322ac571c
sergio0006 <sergio.ana@aluno.faculdadeimpacta.com.br>
2023-09-16 19:00:41 -0300

    criando as instancias do banco de dados do laboratorio.

--- laboratory/models.py
+++ laboratory/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Laboratory(models.Model):
+    name_laboratory = models.CharField(max_length=20)
+    number_laboratory = models.PositiveIntegerField()
+    is_active = models.BooleanField(default=True)
+
+
+class Schedule_Lab(models.Model):
+    lab = models.ForeignKey(Laboratory, on_delete=models.CASCADE)
+    rent_date = models.DateField()
+    available_hour = models.TimeField()
+    rent_hour = models.TimeField()
