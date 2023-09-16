a48e83ecd3087f8eb3afbe64ccc2c7a88ca743b3
sergio0006 <sergio.ana@aluno.faculdadeimpacta.com.br>
2023-09-15 21:45:12 -0300

    redirecionando o usuário para a própria tela de atualização e enviando mensagem de atualização realizada com sucesso.

--- main/views/update_user.py
+++ main/views/update_user.py
@@ -3,9 +3,11 @@ from django.shortcuts import render, redirect
 from ..models import Usuario
 from ..forms.updateuser_forms import UserUpdateForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 
 
 class UpdateUser(LoginRequiredMixin, View):
+
     template_name = 'update_user.html'
 
     def get(self, request, pk):
@@ -13,12 +15,14 @@ class UpdateUser(LoginRequiredMixin, View):
         form = UserUpdateForm(instance=user)
         return render(request, self.template_name, {'form': form})
 
+
     def post(self, request, pk):
         user = Usuario.objects.get(pk=pk)
         form = UserUpdateForm(request.POST, instance=user)
         if form.is_valid():
             form.save()
-            return redirect('dashboard')
+            messages.success(request, 'Atualização realizada com sucesso.')
 
+            return redirect('updateuser', pk=pk)
 
         return render(request, self.template_name, {'form': form})
