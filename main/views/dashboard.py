c03acb81a7e62fda841ed66c64232fab6b1c0be9
sergio0006 <sergio.ana@aluno.faculdadeimpacta.com.br>
2023-09-17 20:55:45 -0300

    atualização da vies para mostrar os laboratórios cadastrados.

--- main/views/dashboard.py
+++ main/views/dashboard.py
@@ -2,12 +2,20 @@ from django.views.generic import View
 from ..models import Usuario
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
+from laboratory.forms.schedule_lab_form import ScheduleLabForm
+from laboratory.models import Laboratory
 
 
 class Dashboard(LoginRequiredMixin, View):
 
     def get(self, request, username):
-        print({'username': 'userna'})
         user = get_object_or_404(Usuario, username=username)
+        lab_active = Laboratory.objects.filter(is_active=True).values('name_laboratory', 'number_laboratory')
+        form = ScheduleLabForm()
+        if lab_active:
 
-        return render(request, 'dashboard.html', {'user': user})
+            context = {'user': user, 'lab_active': lab_active, 'form': form}
+
+            return render(request, 'dashboard.html', context)
+        
+        
